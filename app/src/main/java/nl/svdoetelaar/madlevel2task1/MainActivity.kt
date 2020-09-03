3558b3832e7ae00c82bb5d798e2773824731e6e5
Sander <sandervandenoetelaar@hotmail.com>
2020-09-03 21:16:25 +0200

    Step 2

--- app/src/main/java/nl/svdoetelaar/madlevel2task1/MainActivity.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel2task1/MainActivity.kt
@@ -2,10 +2,21 @@ package nl.svdoetelaar.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import nl.svdoetelaar.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init(){
+
     }
 }
